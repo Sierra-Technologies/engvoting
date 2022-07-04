1dc8b9572e6494494ff3c159512247c88d7fcc82
Emmanuel Kamanda <56160669+Emmanuel1255@users.noreply.github.com>
2022-07-04 00:24:49 +0000

    Update voting.js

--- voting.js
+++ voting.js
@@ -114,7 +114,7 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("clock").innerHTML = "EXPIRED";
+    document.getElementById("clock").innerHTML = "VOTING CLOSE";
   }
 }, 1000);
 
